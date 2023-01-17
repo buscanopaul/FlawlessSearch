d16a65f680e35c55af2af0bf4039426a078ec73b
Paul Buscano <pbuscano@deloitte.com>
2023-01-17 21:43:23 +0800

    Change file structure

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import DetailScreen from "./screens/DetailScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailScreen from "./src/screens/DetailScreen";
 
 const Stack = createNativeStackNavigator();
 
